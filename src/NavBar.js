b99d96a538840e70aaa0595f2bd6d53baa0cb617
artem <artemradzivon@gmail.com>
2019-07-23 11:43:19 +0300

    add simple animation

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
+import { css, keyframes } from "@emotion/core";
 import colors from "./colors";
 
 const NavBar = () => {
   const [padding, setPadding] = useState(15);
+
+  const spin = keyframes`
+    to {
+      transform: rotate(360deg)
+    }
+  `;
 
   return (
     <header
@@ -19,6 +25,13 @@ const NavBar = () => {
       <span
         css={css`
           font-size: 60px;
+          cursor: pointer;
+          display: inline-block;
+          animation: 2s ${spin} linear infinite;
+
+          &:hover {
+            animation: 2s ${spin} linear infinite reverse;
+          }
         `}
         role="img"
         aria-label="logo"

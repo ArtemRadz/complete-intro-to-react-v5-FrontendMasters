9a60cbed04fcf534e82fe09b63e3aabf8fec40da
artem <artemradzivon@gmail.com>
2019-07-25 16:45:09 +0300

    add code splitting

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { lazy } from "react";
 
 import { navigate } from "@reach/router";
 import pet from "@frontendmasters/pet";
 
-import Modal from "./Modal";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+
+const Modal = lazy(() => import("./Modal"));
 
 class Details extends React.Component {
   state = { loading: true, showModal: false };

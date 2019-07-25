bf2cd40e33b02902878a469be42cb9e199240fc7
artem <artemradzivon@gmail.com>
2019-07-25 17:40:34 +0300

    remove emotion

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,26 +1,11 @@
 import React from "react";
 
 import { Link } from "@reach/router";
-import { css } from "@emotion/core";
 
 const NavBar = () => {
   return (
-    <header
-      css={css`
-        padding: 15px;
-      `}
-    >
+    <header>
       <Link to="/">Adopt Me!</Link>
-      <span
-        css={css`
-          font-size: 50px;
-          display: inline-block;
-        `}
-        role="img"
-        aria-label="logo"
-      >
-        🐱‍💻
-      </span>
     </header>
   );
 };
